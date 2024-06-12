a8dab7463a55b8069c24b5c98103aaf913adb46b
omkaran patel <141492286+omakaranpatel@users.noreply.github.com>
2024-06-12 14:06:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,3 @@
-# # LangChain 
-# #Chat Prompt
-# #LLM
-# #Chain 
-# # # LangChain 
-# from langchain.prompts import PromptTemplate
-# from langchain_community.llms import Ollama
-# from langchain.chains import LLMChain
-
-
-# # Chat Prompt
-# data_analyst_template = """You are a data analyst. You are working on a database named customers, which has a table named customers and the table has the following columns,customer_id INT PRIMARY KEY,
-#     first_name VARCHAR(50),
-#     last_name VARCHAR(50),
-#     email VARCHAR(100),
-#     phone VARCHAR(15),
-#     address VARCHAR(255),
-#     city VARCHAR(50),
-#     join_date DATE
-#     \n Your job is to perform the given task {prompt} by converting the promt into sql
-#     queris. the result should be the sql query in string format excluding '''sql'''.
-#     Result: 
-#     """
-
-# data_analyst_prompt_template = PromptTemplate(
-#     input_variables=["prompt"],
-#     template=data_analyst_template
-# )
-
-
-# # LLM
-# gemma = Ollama(model="llama3")
-
-
-# # Chain
-# llm_chain = LLMChain(llm=gemma,prompt=data_analyst_prompt_template)
-
-# def query_llm(prompt):
-#     print(llm_chain.invoke({"prompt":prompt})["text"])
-
-# query_llm("Give me the names of the customers from Benguluru")
-# def extract_sql_code(input_string):
-#     sql_code = input_string.replace("'''sql", "").replace("'''", "").strip()
-#     return sql_code
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import Ollama
 from langchain.chains import LLMChain
@@ -77,7 +33,7 @@ def extract_sql_code(input_string):
     return sql_code
 
 def query_db(sql_query):
-    con = ms.connect(host="localhost",user="root",password="root",database="Company")
+    con = ms.connect(host="localhost",user="####",password="####",database="Company")
     cursor = con.cursor()
     
     cursor.execute(sql_query)
